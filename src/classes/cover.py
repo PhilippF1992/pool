5bc0f8ad623f03c745acd355a73750cfacabd675
PhilippF1992 <philipp@bendesk.de>
2023-05-14 01:13:51 +0200

    Cover to normal switch

--- src/classes/cover.py
+++ src/classes/cover.py
@@ -1,7 +1,8 @@
 import paho.mqtt.client as mqtt
+import time
 from .device import *
 from .binary_sensors.normal import *
-from .switches.auto_off import *
+from .switches.normal import *
 
 class Cover:
     def __init__(self, uniq_id, device: Device, client: mqtt.Client, gpio_cover_closed, gpio_cover_opened, gpio_cover_closing, gpio_cover_opening, gpio_cover_impuls, connect_on):
@@ -10,7 +11,7 @@ class Cover:
         self.cover_opened = Binary("Pool Cover Opened", "pool_cover_opened", device, client, gpio_cover_opened, uniq_id)
         self.cover_closing = Binary("Pool Cover Closing", "pool_cover_closing", device, client, gpio_cover_closing, uniq_id)
         self.cover_opening = Binary("Pool Cover Opening", "pool_cover_opening", device, client, gpio_cover_opening, uniq_id)
-        self.cover_impuls = AutoOff("Pool Cover Impuls", "pool_cover_impuls", device, client, gpio_cover_impuls, connect_on, uniq_id)
+        self.cover_impuls = Switch("Pool Cover Impuls", "pool_cover_impuls", device, client, gpio_cover_impuls, connect_on, uniq_id)
     
     def send_data(self):
         self.cover_closed.send_data()
@@ -19,4 +20,9 @@ class Cover:
         self.cover_opening.send_data()
 
     def on_message(self, message):
-        self.cover_impuls(message)
+        payload=str(message.payload.decode("utf-8"))
+        if (self.cover_impuls.uniq_id in message.topic):
+            if (payload=="True"):
+                self.cover_impuls.set_on()
+                time.sleep(0.5)
+                self.cover_impuls.set_off()
